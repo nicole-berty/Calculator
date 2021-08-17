3e5ceb82677fcd7a408bb152642ff226d4a80d42
Nicole Berty <nicole.berty@gmail.com>
2021-08-17 19:38:24 +0100

    Add reset function to reset operator and operands. Update code for calculating and displaying to fix bug that caused incorrect output

--- script.js
+++ script.js
@@ -22,6 +22,7 @@ function operate(operator, op1, op2) {
     var answer;
     var a = parseFloat(op1);
     var b = parseFloat(op2);
+    console.log("IN OPERATE " + op2);
     console.log("op is  " + operator + " a is " + a + " b is " + b);
     switch(operator) {
         case "+":
@@ -41,25 +42,28 @@ function operate(operator, op1, op2) {
 }
 
 function display(value) {
+    console.log("BEFORE OP1 " + operand1);
+    console.log("OP2 " + operand2);
+    console.log("OP " + operator);
     if(document.getElementById("result").value == null || document.getElementById("result").value == 0 
     || document.getElementById("result").value == "" || document.getElementById("result").value == "0"){
         document.getElementById("result").value = value;
     } else if(value == "+" || value == "*" || value == "/" || value == "-") {
+        if(operator == "") {
+            operator = value;
+        }
         if(operand1 == 0) {
             var display = document.getElementById("result").value;
             const pieces = display.split(value);
             operand1 = pieces[0];
-            operator = value;
-            console.log("OPERATOR " + operator);
+          //  console.log("OPERATOR " + operator);
         } else if(operand2 == 0) {
             var display = document.getElementById("result").value;
             const pieces = display.split(value);
             operand2 = pieces[1];
-            console.log("op " + operator);
+            console.log("pieces  " + pieces[0]);
             document.getElementById("result").value = operate(operator, operand1, operand2)
-            operand1 = document.getElementById("result").value;
-            operand2 = 0;
-            console.log("op2 " + operator);
+            reset();
         }
         document.getElementById("result").value += value;
         console.log("1 " + operand1);
@@ -67,11 +71,14 @@ function display(value) {
     } else {
         document.getElementById("result").value += value;
     }
-    
+    console.log("AFTER OP1 " + operand1);
+    console.log("OP2 " + operand2);
+    console.log("OP " + operator);    
 }
 
 function clr() {
     document.getElementById("result").value = "0";
+    reset(0);
 }
 
 function del() {
@@ -81,6 +88,28 @@ function del() {
         document.getElementById("result").value = str;
     } else if(str.length == 1) {
         document.getElementById("result").value = "0";
+    }
+}
+
+function reset(value) {
+    if(value != null) {
+        operand1 = 0; 
+    } else {
+        operand1 = document.getElementById("result").value;
+    }
+    operand2 = 0;
+    operator = "";
+}
+function calculate() {
+    if(operand2 == 0) {
+        var display = document.getElementById("result").value;
+        const pieces = display.split(/[^0-9]/);
+        operand2 = pieces[1];
+        document.getElementById("result").value = operate(operator, operand1, operand2);
+        reset();
+    } else {
+        document.getElementById("result").value = operate(operator, operand1, operand2)
+        reset();
     }
 }
 
